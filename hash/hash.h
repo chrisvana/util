492831662fc2250bb9382a7d116efd2cd8904205
Chris Van Arsdale <chris.vana@gmail.com>
2015-02-06 15:12:37 -0800

    #define

--- hash/hash.h
+++ hash/hash.h
@@ -1,6 +1,9 @@
 // Copyright 2015
 // Author: Christopher Van Arsdale
 
+#ifndef _THIRD_PARTY_UTIL_HASH_HASH_H__
+#define _THIRD_PARTY_UTIL_HASH_HASH_H__
+
 #include "third_party/util/third_party/google/or_tools/base/hash.h"
 
 namespace util {
@@ -9,3 +12,5 @@ namespace util {
 // see "util/third_party/google/or_tools/base/util.h" for hash methods.
 
 }  // namespace util
+
+#endif  // _THIRD_PARTY_UTIL_HASH_HASH_H__
--- random/random.h
+++ random/random.h
@@ -1,6 +1,9 @@
 // Copyright 2015
 // Author: Christopher Van Arsdale
 
+#ifndef _THIRD_PARTY_UTIL_RANDOM_RANDOM_H__
+#define _THIRD_PARTY_UTIL_RANDOM_RANDOM_H__
+
 #include "third_party/util/third_party/google/or_tools/base/random.h"
 
 namespace util {
@@ -12,3 +15,5 @@ typedef operations_research::MTRandom MTRandom;
 // see "util/third_party/google/or_tools/base/util.h" for ACMRandom object.
 
 }  // namespace util
+
+#endif  // _THIRD_PARTY_UTIL_RANDOM_RANDOM_H__
